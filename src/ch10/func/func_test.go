c3178559b1d7e489534abd4b0ed3e616c923e668
panyao.yang <panyao.yang@hand-china.com>
2020-12-01 14:29:06 +0800

    Update func_test.go

--- src/ch10/func/func_test.go
+++ src/ch10/func/func_test.go
@@ -12,9 +12,10 @@ func returnMultiValues() (int, int) {
 }
 
 func TestFn(t *testing.T)  {
+	//多个返回值
 	a,b :=returnMultiValues()
 	t.Log(a,b)
-
+	//求运行时长
 	tsSF :=TimeSpent(slowFn)
 	t.Log(tsSF(10))
 }
@@ -31,4 +32,31 @@ func TimeSpent(inner func(op int) int) func(op int) int  {
 func slowFn(op int) int {
 	time.Sleep(time.Second * 1)
 	return op
+}
+
+func TestVarParam(t *testing.T)  {
+	t.Log(Sum(1,2,3,4,5))
+}
+
+
+
+//可变长参数
+func Sum(ops ...int) int  {
+	ret := 0
+	for _,op := range ops{
+		ret += op
+	}
+	return ret
+}
+
+
+//defer 函数延迟执行 通常用于释放清理资源等
+func TestDefer(t *testing.T)  {
+	defer Clear()
+	fmt.Println("start")
+	panic("Fatal error")//defer仍会执行
+}
+
+func Clear()  {
+	fmt.Println("Clear resources")
 }
